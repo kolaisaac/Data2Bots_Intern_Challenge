28e5da6cfe6d7c31d8341f445fafed58b382f9e0
kolaisaac <blestisaac10@gmail.com>
2021-07-20 09:17:34 +0100

    Test Added

---
+++ main_test.py
@@ -0,0 +1,15 @@
+from challenge import obsolete_flaging
+
+def test_obsolete_flaging_expired():
+
+	""" Ensuring that obsolete_flaging function returns TRUE for All items with a date before 2021-01-01 are expired
+	"""
+	expired = obsolete_flaging('2020-12-22')
+	assert expired == 'TRUE'
+
+def test_obsolete_flaging_not_expired():
+
+	""" Ensuring that obsolete_flaging function returns FALSE for All items with a date after 2021-01-01 are not expired
+	"""
+	expired = obsolete_flaging('2021-06-12')
+	assert expired == 'FALSE'
