17db3e6b6e73370b194dfb554381ce57ff32c462
kolaisaac <blestisaac10@gmail.com>
2021-07-21 15:05:21 +0100

    Test updated with unittest :fire: :rocket:

--- main_test.py
+++ main_test.py
@@ -1,18 +1,22 @@
+
+import unittest
 from challenge import check_expiration
-import pandas as pd
+from challenge import data_to_json
 
-def test_obsolete_flaging_expired():
+class DataJson(object):
+	"""docstring for DataJson"""
 
-	""" Ensuring that obsolete_flaging function returns TRUE for All items with a date before 2021-01-01 are expired
-	"""
-	item_date = pd.to_datetime('2020-12-22')
-	expired = check_expiration(item_date)
-	assert expired == 'TRUE', "Should be TRUE"
-
-def test_obsolete_flaging_not_expired():
-
-	""" Ensuring that obsolete_flaging function returns FALSE for All items with a date after 2021-01-01 are not expired
-	"""
-	item_date = pd.to_datetime('2021-01-01')
-	expired = check_expiration(item_date)
-	assert expired == 'FALSE', "Should be FALSE"
+	def data_with_obsolete_column_json(self):
+        #Confirm that the program only takes csv files
+        file = 'data_updated.json'
+        expired = check_expiration(file)
+        self.assertFalse(expired == True)
+        
+    @unittest.expectedFailure
+    def data_with_obsolete_column_csv(self):
+        file = 'dataset.csv'
+        expired = check_expiration(file)
+        self.assertTrue(expired  == True)
+        
+if __name__ == '__main__':
+    unittest.main()
