d976667bb7a4364f230996982e4c5100a8e8ff54
kolaisaac <blestisaac10@gmail.com>
2021-07-21 15:24:51 +0100

    IndentationError fixed :fire: :rocket:

--- main_test.py
+++ main_test.py
@@ -7,14 +7,15 @@ class DataJson(object):
 	"""docstring for DataJson"""
 
 	def data_with_obsolete_column_json(self):
-        #Confirm that the program only takes csv files
-        file = 'data_updated.json'
+
+		#Confirm that the program only takes csv files
+		file = 'data_updated.json'
         expired = check_expiration(file)
         self.assertFalse(expired == True)
         
     @unittest.expectedFailure
     def data_with_obsolete_column_csv(self):
-        file = 'dataset.csv'
+    	file = 'dataset.csv'
         expired = check_expiration(file)
         self.assertTrue(expired  == True)
         
