518912a580da860e01d261b50b374550f5aee8d6
kolaisaac <blestisaac10@gmail.com>
2021-07-21 02:00:49 +0100

    Dates using Pandas :fire: :rocket:

--- main_test.py
+++ main_test.py
@@ -1,16 +1,18 @@
 from challenge import check_expiration
-
+import pandas as pd
 
 def test_obsolete_flaging_expired():
 
 	""" Ensuring that obsolete_flaging function returns TRUE for All items with a date before 2021-01-01 are expired
 	"""
-	expired = obsolete_flaging('2020-12-22')
+	item_date = pd.to_datetime('2020-12-22')
+	expired = check_expiration(item_date)
 	assert expired == 'TRUE', "Should be TRUE"
 
 def test_obsolete_flaging_not_expired():
 
 	""" Ensuring that obsolete_flaging function returns FALSE for All items with a date after 2021-01-01 are not expired
 	"""
-	expired = obsolete_flaging('2021-06-12')
+	item_date = pd.to_datetime('2021-01-01')
+	expired = check_expiration(item_date)
 	assert expired == 'FALSE', "Should be FALSE"
