3dec0369bf18ac967caa46cc80f3e521a0604d81
kolaisaac <blestisaac10@gmail.com>
2021-07-21 15:38:36 +0100

    indent remove :fire: :rocket:

--- main_test.py
+++ main_test.py
@@ -5,18 +5,18 @@ from challenge import data_to_json
 
 class DataJson(object):
 	"""docstring for DataJson"""
+	def data_with_obsolete_cloumn_json(self):
 
-	def data_with_obsolete_column_json(self):
-
-		#Confirm that the program only takes csv files
+		#Confirm that the ptogram only takes csv files
 		file = 'data_updated.json'
-        expired = check_expiration(file)
-        self.assertFalse(expired == True)
+		expired = check_expiration(file)
+		self.assertFalse(expired == True)
 
-    def data_with_obsolete_column_csv(self):
-    	file = 'dataset.csv'
-        expired = check_expiration(file)
-        self.assertTrue(expired  == True)
-        
+	def data_with_obsolete_cloumn_csv(self):
+
+		file = 'dataset.csv'
+		expired = check_expiration(file)
+		self.assertTrue(expired == True)
+
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
